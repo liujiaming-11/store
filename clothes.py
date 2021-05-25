436640e5f57c559f6481c2f52aa4bf868cf7034d
liujiaming-11 <84754505+liujiaming-11@users.noreply.github.com>
2021-05-25 16:30:54 +0800

    Add files via upload

---
+++ clothes.py
@@ -0,0 +1,28 @@
+
+id1 = 1
+name1 =  "毛衣"
+price1 =  152
+num1 =  150
+add1 =  "河北"
+quan1 =  "A+"
+
+id2 = 2
+name2 = "毛裤"
+price2 = 200
+num2 = 160
+add2 = "海南"
+quan2 = "SSS+"
+
+id3 = 3
+name3 = "裤衩"
+price3 = 99
+num3 = 9999
+add3 = "泰国"
+quan3 = "Z-"
+
+print("===========小刘黑心死啦贵买不起的服装店开业了============")
+print("编号    名称    价格    数量    产地   品质")
+print(id1,"   ",name1,"   ",price1,"   ",num1,"   ",add1,"   ",quan1)
+print(id2,"   ",name2,"   ",price2,"   ",num2,"   ",add2,"   ",quan2)
+print(id3,"   ",name3,"   ",price3,"   ",num3,"  ",add3,"  ",quan3)
+print("总金额:￥",(price1*num1+price2*num2+price3+num3))
