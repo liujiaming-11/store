79330a243d8e8db5f43170dd77866d1d31fd5eca
liujiaming-11 <84754505+liujiaming-11@users.noreply.github.com>
2021-05-27 09:34:28 +0800

    Add files via upload

---
+++ baofu.py
@@ -0,0 +1,146 @@
+# import random
+# pp = random.randint(50,150)
+# print (pp)
+
+
+# A = int(input("输入边A:"))
+# B = int(input("输入边B:"))
+# C = int(input("输入边C:"))
+#
+# if A+B>C and A+C>B and B+C>A:
+#     print("构成三角形")
+#     if A*A+B*B==C*C or A*A+C*C==B*B or B*B+C*C==A*A:
+#         print("直角三角形")
+#     elif A==B or A==C or B==C:
+#         print("构成等腰三角形")
+#         if A==B==C:
+#             print("等边三角形")
+# else:
+#     print("去去去，不是三角形。")
+
+
+
+
+# count = 0
+# while True:
+#     name = input("请输入用户名：")
+#     password = input("请输入密码：")
+#     count = count +1
+#     if name == "root" and password == "admin":
+#         print("芝麻开门")
+#         break
+#     elif count == 3 :
+#         print("锁住")
+#         break
+#     else:
+#         print("乱搞！")
+
+
+
+
+
+
+
+
+# v = 1
+# while v<=100:
+#     print(v)
+#     v=v+1
+
+
+
+
+
+
+
+
+# nun = int(input("嘿嘿嘿，输入一个数："))
+# nun1 = int(input("嘿嘿嘿，输入一个数："))
+# nun2 = int(input("嘿嘿嘿，输入一个数："))
+# nun3 = int(input("嘿嘿嘿，输入一个数："))
+# nun4 = int(input("嘿嘿嘿，输入一个数："))
+# nun5 = int(input("嘿嘿嘿，输入一个数："))
+# nun6 = int(input("嘿嘿嘿，输入一个数："))
+# nun7 = int(input("嘿嘿嘿，输入一个数："))
+# nun8 = int(input("嘿嘿嘿，输入一个数："))
+# nun9 = int(input("嘿嘿嘿，输入一个数："))
+# pz = nun+nun1+nun2+nun3+nun4+nun5+nun6+nun7+nun8+nun9
+# print(nun9)
+# print(pz/10)
+# print("总数=",(nun+nun1+nun2+nun3+nun4+nun5+nun6+nun7+nun8+nun9))
+
+
+# print("=================一夜暴富系统开业了=================")
+# jb =5000
+# import random
+# jl = random.randint(0,100)
+# cccot = 0
+# ooop= 0
+# while True:
+#     nn = input("请猜猜中奖号码：")
+#     nn = int(nn)
+#     jb = jb -100
+#     cccot = cccot+1
+#     ooop = ooop + 1
+#     if ooop == 10:
+#         print("次数用尽了！跟你讲！下次再来！")
+#     if nn > jl:
+#         print("大了大了！")
+#     elif nn < jl:
+#         print("小了小了！")
+#     if nn == jl:
+#         print("可恶，被猜中了")
+#         print("金币：" , jb)
+#         print("次数：",cccot)
+#         if cccot<=10:
+#             jb = jb+1000000000
+#             print("奖励拿走！")
+#             print("金币" , jb )
+#             print("次数：" , cccot)
+#         break
+#     elif jb==0:
+#         print("没钱了吧？充值！")
+#         break
+
+
+
+# print("===========不甘心坐井观天的老青蛙============")
+# m = 0
+# m1 = 3
+# m2 = 2
+# date = 0
+# while True:
+#     date = date + 1
+#     m = m + m1
+#     if m >= 20:
+#         print("享受你的蓝天，老青蛙。" , date , "天")
+#         break
+#     m = m - m2
+#     if m >= 20:
+#         print("享受你的蓝天，老青蛙。" , date ,"天")
+#         break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
